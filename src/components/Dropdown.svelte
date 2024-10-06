22969b8263eb0c89f5ffa5dccbdf3f062d9e81dd
1ucyhan <lucehanblank@gmail.com>
2024-10-05 23:15:05 -0500

    dropdown implementation

---
+++ src/components/Dropdown.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { createEventDispatcher, onMount } from 'svelte';
+    let breeds = [];
+    let selectedBreed = '';
+  
+    // Create a dispatch.
+    const dispatch = createEventDispatcher();
+  
+    // Fetch (haha get it) all the dog breeds.
+    // Make sure this is on Mount
+    onMount(async () => {
+      const res = await fetch('https://dog.ceo/api/breeds/list/all');
+      const data = await res.json();
+      console.log(Object.keys(data.message))
+      breeds = Object.keys(data.message);
+    });
+
+    // Selecting the breed of the dog
+    function selectBreed(event) {
+      selectedBreed = event.target.value;
+      dispatch('select', selectedBreed);
+    }
+  </script>
+  
+  <select
+    bind:value={selectedBreed}
+    on:change={selectBreed}
+    class="mt-4 p-2 rounded-xl border bg-fetchpurple text-white text-sm w-[200px] "
+  >
+    <option value="" disabled>Select a breed</option>
+    {#each breeds as breed}
+      <option value={breed}>{breed}</option>
+    {/each}
+  </select>
+
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
 <script>
+	import Dropdown from '../components/Dropdown.svelte';
+	let dogImages = [];
+
+	// Fetch images when a breed is selected
+	async function fetchImages(breed) {
+		const res = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+		const data = await res.json();
+		dogImages = data.message.slice(0, 10); // Display the first 10 images
+	}
+
+	// Handle the breed selection
+	function handleBreedSelect(event) {
+		const selectedBreed = event.detail; // The breed selected from the dropdown
+		fetchImages(selectedBreed);
+	}
 
 </script>
 
@@ -12,11 +27,29 @@
 </svelte:head>
 
 <section>
-	<div class="flex flex-col justify-center items-center min-h-screen">
-		<h1 class = "text-6xl font-extrabold text-fetchpurple animate__animated animate__rotateInUpRight"> <span class = "text-fetchorange animate__animated animate__rotateInUpRight"> Fetch </span> me a doggy. 🐶 </h1>
+	<div class="flex flex-col justify-center items-center pt-[20%]">
+		<h1 class = "text-6xl font-extrabold text-fetchpurple animate__animated animate__rotateInUpRight"> 
+			<span class = "text-fetchorange animate__animated animate__rotateInUpRight"> Fetch </span> 
+			me a doggy. 🐶 </h1>
 		<p class = "pt-8 text-color-black"> Click on the dropdown below to see some cute doggies... </p>
+		<Dropdown on:select={handleBreedSelect} />
+	
+
+	<!-- Dropdown -->
+
+	<!-- Displaying the selected breed -->
 	</div>
+
+	<div class="grid grid-cols-3 gap-4 mt-8">
+		{#each dogImages as image}
+			<img src={image} alt="Dog image" class="w-full h-auto" />
+		{/each}
+	</div>
+
 </section>
+
+
+  
 
 <style>
 	
