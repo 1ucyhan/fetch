ce9b71eb68aed0c7058cb0173c73a36d78cea6db
1ucyhan <lucehanblank@gmail.com>
2024-10-07 09:36:38 -0500

    fix up codebase for Dropdown

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -1,36 +1,45 @@
+<!-- NOTE THAT THIS COMPONENT IS NOT BEING USED ANYMORE! This is simply for my reference for my past code.-->
 <script lang="ts">
-    import { createEventDispatcher, onMount } from 'svelte';
-    let breeds: string[] = [];
-    let selectedBreed = '';
+  // Importing elements from svelte.
+  import { createEventDispatcher, onMount } from 'svelte';
+  // Geting an array of all the breeds.
+  let breeds: string[] = [];
+  // Displaying the selected breed here.
+  let selectedBreed = '';
   
-    // Create a dispatch.
-    const dispatch = createEventDispatcher();
+  // Create a dispatch.
+  const dispatch = createEventDispatcher();
   
-    // Fetch (haha get it) all the dog breeds.
-    // Make sure this is on Mount
-    onMount(async () => {
-      const res = await fetch('https://dog.ceo/api/breeds/list/all');
-      const data = await res.json();
-      console.log(Object.keys(data.message))
-      breeds = Object.keys(data.message);
-    });
+  // Fetch (haha get it) all the dog breeds.
+  // Make sure this is on Mount.
+  onMount(async () => {
+    const res = await fetch('https://dog.ceo/api/breeds/list/all');
+    const data = await res.json();
+    console.log(Object.keys(data.message))
+    breeds = Object.keys(data.message);
+  });
 
-    // Selecting the breed of the dog
-    function selectBreed(event: Event) {
-      selectedBreed = event.target.value;
+  // Selecting the breed of the dog.
+  function selectBreed(event: Event) {
+    const target = event.target as HTMLInputElement | null;
+    if (target) {
+      selectedBreed = target.value;
       dispatch('select', selectedBreed);
     }
-  </script>
-  
-  <select
-    bind:value={selectedBreed}
-    on:change={selectBreed}
-    class="mt-4 p-2 rounded-xl border bg-fetchpurple text-white text-sm w-[200px] "
-  >
-    <option value="" disabled>Select a breed</option>
-    {#each breeds as breed}
-      <option value={breed}>{breed}</option>
-    {/each}
+  }
+</script>
+
+<!-- Displaying the selected values. -->
+<select
+  bind:value={selectedBreed}
+  on:change={selectBreed}
+  class="mt-4 p-2 rounded-xl border bg-fetchpurple text-white text-sm w-[200px] "
+>
+  <!-- Displaying each possible value. -->
+  <option value="" disabled>Select a breed</option>
+  {#each breeds as breed}
+    <option value={breed}>{breed}</option>
+  {/each}
 </select>
 
   
